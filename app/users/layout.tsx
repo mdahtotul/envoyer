ce8e90786321b54cbd1d66d2ebbc8a07b808e060
mdahtotul <mdahtotul@gmail.com>
2023-06-29 00:30:07 +0600

    sidebar navigation done

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -7,7 +7,7 @@ export default async function UsersLayout({
 }) {
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>;
+      <div className="h-full">{children}</div>
     </Sidebar>
   );
 }
