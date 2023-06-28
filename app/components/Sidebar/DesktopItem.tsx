c1b09df45c1e19986ff8a3262537dfe23255bb59
mdahtotul <mdahtotul@gmail.com>
2023-06-28 22:35:52 +0600

    some style changed

--- app/components/Sidebar/DesktopItem.tsx
+++ app/components/Sidebar/DesktopItem.tsx
@@ -29,7 +29,7 @@ const DesktopItem: React.FC<DesktopItemProps> = ({
         href={href}
         className={clsx(
           `group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100`,
-          active && `bg-gray-100 text-black`
+          active && `bg-gray-100 text-gray-950`
         )}
       >
         <Icon className="h-6 w-6 shrink-0" />
